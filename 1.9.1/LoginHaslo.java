4d77ab93bce485b83bc0fb315e6e43a579a37c81
Gadendd <77695543+Gadendd@users.noreply.github.com>
2021-03-30 09:43:05 +0200

    1.9.1 zad 2

---
+++ 1.9.1/LoginHaslo.java
@@ -0,0 +1,12 @@
+	
+	class LoginHaslo{
+		private String haslo = "kekW2137";
+		private String login = "qwerty";
+		
+		
+		@override
+		public String toString(){
+			return haslo+" "+login;
+		}
+	}
+	
