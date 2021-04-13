0f9b4e03decf766e9019a79ab02869f018120b44
Gadendd <77695543+Gadendd@users.noreply.github.com>
2021-04-13 09:38:40 +0200

    Update trojkat.java

--- trojkat/trojkat.java
+++ trojkat/trojkat.java
@@ -5,8 +5,20 @@ class trojkat{
 		
 		
 		
-		public trojkat{
-			
+		public trojkat(int wysokosc, int podstawa){
+			System.out.println(wysokosc*podstawa/2);
 		}	
 	}
 }
+
+	━━╮
+╰┃ ┣▇━▇
+ ┃ ┃  ╰━▅╮
+ ╰┳╯ ╰━━┳╯ BRUV KEK😎
+  ╰╮ ┳━━╯ SUPER EASY🤙
+ ▕▔▋ ╰╮╭━╮NICE TUTORIAL😂
+╱▔╲▋╰━┻┻╮╲╱▔▔▔╲
+▏  ▔▔▔▔▔▔▔  O O┃
+╲╱▔╲▂▂▂▂╱▔╲▂▂▂╱
+ ▏╳▕▇▇▕ ▏╳▕▇▇▕
+ ╲▂╱╲▂╱ ╲▂╱
