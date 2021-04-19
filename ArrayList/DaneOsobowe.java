6bbb82e61af79efefee6e65f7814e812783dd45f
Gadendd <77695543+Gadendd@users.noreply.github.com>
2021-04-19 08:45:57 +0200

    19/04/21 cwiczenia

---
+++ ArrayList/DaneOsobowe.java
@@ -0,0 +1,16 @@
+class DaneOsobowe{
+	private String name = "";
+	private String lastname = "";
+	private int age = 0;
+	private int height = 0;
+	private int weight = 0;
+	}
+	
+	public DaneOsobowe(String name, String lastname, int age, int height, int weight) {
+		this.name = name;
+		this.lastname = lastname;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+		
+	}
---
+++ ArrayList/Main.java
@@ -0,0 +1,25 @@
+
+import java.utill.ArrayList;
+
+class Main{
+    public static void main(String[] args){
+		int t[] = new int [10];
+		ArrayList<Integer> lista = new ArrayList<>();
+		
+		for int (i=0;i<10;i++){
+			t[1] = 1+10;
+		}
+		for int (i=0;i<10;i++){
+			lista.add(i+10);
+		}
+		System.out.println(litsa);
+		lista.add(100);
+		t[10] = 100;
+		
+		System.out.println(litsa);
+		lista.add(100);
+		for (int i=0;i<lista.size();i++){
+			System.out.println("Element: "+i+"+lista.get(i));
+		}
+	}
+}
